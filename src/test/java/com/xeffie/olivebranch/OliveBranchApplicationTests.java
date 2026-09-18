0e315970526f7e62378811ad551bf59b76584f4b
xeffie <josefin.vingeskog@hotmail.com>
2026-09-18 14:48:32 +0200

    chore: initial spring boot setup

---
+++ src/test/java/com/xeffie/olivebranch/OliveBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xeffie.olivebranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliveBranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
