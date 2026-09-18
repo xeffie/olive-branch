a7789713f1779654994968cb8abb6ec46237c6b1
Josefin Vingeskog <151967701+xeffie@users.noreply.github.com>
2026-09-18 15:21:41 +0200

    feat: add aid organization model and repository (#1)

---
+++ src/main/java/com/xeffie/olivebranch/repository/AidOrganizationRepository.java
@@ -0,0 +1,7 @@
+package com.xeffie.olivebranch.repository;
+
+import com.xeffie.olivebranch.model.AidOrganization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AidOrganizationRepository extends JpaRepository<AidOrganization, Long> {
+}
